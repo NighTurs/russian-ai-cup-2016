2b41b50a7a6d46096fbaca0daa9f02fc3bd072f3
NighTurs <nighturs@gmail.com>
2016-11-15 23:23:19 +0300

    Better lane moves

--- src/main/java/MapUtils.java
+++ src/main/java/MapUtils.java
@@ -18,22 +18,88 @@ public class MapUtils {
     private final double worldWidth;
     private final double worldHeight;
 
+    private final Point laneAllyWaypoint;
+    private final Point laneEnemyWaypoint;
+    private final Point bottomLaneMidWaypoint;
+    private final Point topLaneMidWaypoint;
+    private final Point midLaneMidWaypoint;
+
+
     public MapUtils(WorldProxy world, Game game) {
         this.store = new HashMap<>();
         List<Unit> units = world.allUnits();
 
         Building allyBase = world.allyBase();
-        this.allyBaseXRight = allyBase.getX() * 2;
+        double halfBase = allyBase.getX();
+
+        this.allyBaseXRight = halfBase * 2;
         this.allyBaseYTop = world.getHeight() - (world.getHeight() - allyBase.getY()) * 2;
         this.enemyBaseXLeft = world.getWidth() - allyBaseXRight;
         this.enemyBaseYBottom = (world.getHeight() - allyBase.getY()) * 2;
-        this.laneWidth = allyBaseXRight * 2 / 3;
+        this.laneWidth = halfBase * 3 / 2;
         this.worldWidth = world.getWidth();
         this.worldHeight = world.getHeight();
+        this.laneAllyWaypoint = new Point(halfBase, worldHeight - halfBase);
+        this.laneEnemyWaypoint = new Point(worldWidth - halfBase, halfBase);
+        this.bottomLaneMidWaypoint = new Point(worldWidth - halfBase, worldHeight - halfBase);
+        this.topLaneMidWaypoint = new Point(halfBase, halfBase);
+        this.midLaneMidWaypoint = new Point(worldWidth / 2, worldHeight / 2);
 
         for (Unit unit : units) {
 
             store.put(unit.getId(), getLocationType(unit.getX(), unit.getY()));
+        }
+    }
+
+    public Point pushWaypoint(double x, double y, LocationType lane) {
+        LocationType curLocationType = getLocationType(x, y);
+        switch (lane) {
+            case MIDDLE_LANE:
+                if (curLocationType != lane) {
+                    return midLaneMidWaypoint;
+                }
+                return laneEnemyWaypoint;
+            case TOP_LANE:
+                if (curLocationType != lane) {
+                    return topLaneMidWaypoint;
+                }
+                if (y > laneWidth) {
+                    return topLaneMidWaypoint;
+                } else {
+                    return laneEnemyWaypoint;
+                }
+            case BOTTOM_LANE:
+                if (curLocationType != lane) {
+                    return bottomLaneMidWaypoint;
+                }
+                if (x < worldWidth - laneWidth) {
+                    return bottomLaneMidWaypoint;
+                } else {
+                    return laneEnemyWaypoint;
+                }
+            default:
+                throw new RuntimeException(lane + " is not a lane");
+        }
+    }
+
+    public Point retreatWaypoint(double x, double y, LocationType lane) {
+        switch (lane) {
+            case MIDDLE_LANE:
+                return laneAllyWaypoint;
+            case TOP_LANE:
+                if (x < laneWidth) {
+                    return laneAllyWaypoint;
+                } else {
+                    return topLaneMidWaypoint;
+                }
+            case BOTTOM_LANE:
+                if (y > worldHeight - laneWidth) {
+                    return laneAllyWaypoint;
+                } else {
+                    return bottomLaneMidWaypoint;
+                }
+            default:
+                throw new RuntimeException(lane + " is not a lane");
         }
     }
 
@@ -62,5 +128,13 @@ public class MapUtils {
         } else {
             return LocationType.FOREST;
         }
+    }
+
+    public Point getLaneAllyWaypoint() {
+        return laneAllyWaypoint;
+    }
+
+    public Point getLaneEnemyWaypoint() {
+        return laneEnemyWaypoint;
     }
 }
--- src/main/java/PushLaneTacticBuilder.java
+++ src/main/java/PushLaneTacticBuilder.java
@@ -1,62 +1,55 @@
-import model.Faction;
-import model.Unit;
-import model.Wizard;
+import model.*;
 
 import java.util.Optional;
 
 public class PushLaneTacticBuilder implements TacticBuilder {
-    private static final int SAFE_DISTANCE_MIN = 100;
-    private static final int SAFE_DISTANCE_MAX = 200;
+    private static final int ENEMY_MINION_MAX_DIST = 350;
+    private static final int ENEMY_MIN_DIST_VS_XP_RANGE = 200;
+    private static final int TOWER_DANGER_RANGE_INC = 20;
+    private static final int TOWER_TARGETS_THRESHOLD = 1;
 
     @Override
     public Optional<Tactic> build(TurnContainer turnContainer) {
-        WorldProxy world = turnContainer.getWorldProxy();
         LocationType lane = turnContainer.getLanePicker().myLane();
+        MapUtils mapUtils = turnContainer.getMapUtils();
         Wizard self = turnContainer.getSelf();
 
-        Optional<Unit> offensiveOpt = findClosestOffensiveUnit(lane, turnContainer);
-        Point offensivePoint = offensiveOpt.isPresent() ?
-                new Point(offensiveOpt.get().getX(), offensiveOpt.get().getY()) :
-                new Point(world.getWidth(), 0);
-        Optional<Unit> allyOpt = findClosestAllyUnit(offensivePoint, lane, turnContainer);
-        if (!allyOpt.isPresent()) {
-            return Optional.empty();
-        }
-        Point allyPoint = new Point(allyOpt.get().getX(), allyOpt.get().getY());
+        boolean enemiesNearby = hasEnemyInVisibilityRange(turnContainer);
+        Action action = Action.NONE;
 
-        double retreatX = world.allyBase().getX();
-        double retreatY = world.allyBase().getY();
-        double measureUnit = world.allyBase().getX();
-        if (lane == LocationType.BOTTOM_LANE && self.getY() < 4 * measureUnit) {
-            retreatX = world.getWidth() - measureUnit;
-            retreatY = world.getHeight() - measureUnit;
-        }
-
-        if (lane == LocationType.TOP_LANE && self.getX() > world.getWidth() - 4 * measureUnit) {
-            retreatX = measureUnit;
-            retreatY = measureUnit;
-        }
-
-        double distWizard = dist(self.getX(), self.getY(), retreatX, retreatY);
-        double distEnemy = dist(offensivePoint.getX(), offensivePoint.getY(), retreatX, retreatY);
-        double distAlly = dist(allyPoint.getX(), allyPoint.getY(), retreatX, retreatY);
-        double distWizAlly = dist(self.getX(), self.getY(), allyPoint.getX(), allyPoint.getY());
-        double distWizEnemy = dist(self.getX(), self.getY(), offensivePoint.getX(), offensivePoint.getY());
-        double distAllyEnemy =
-                dist(allyPoint.getX(), allyPoint.getY(), offensivePoint.getX(), offensivePoint.getY());
-
-        Movement mov;
-        if (distWizard <= distAlly && distAllyEnemy <= distEnemy) {
-            double distSafety = distWizAlly / (distWizAlly + distAllyEnemy) * distWizEnemy;
-            if (distSafety > SAFE_DISTANCE_MAX) {
-                mov = turnContainer.getPathFinder().findPath(self, allyPoint.getX(), allyPoint.getY());
-            } else if (distSafety < SAFE_DISTANCE_MIN) {
-                mov = turnContainer.getPathFinder().findPath(self, retreatX, retreatY);
-            } else {
-                return Optional.empty();
+        if (enemiesNearby) {
+            if (shouldRetreatBecauseOfMinions(lane, turnContainer) ||
+                    shouldRetreatBecauseOfBuildings(lane, turnContainer)) {
+                action = Action.RETREAT;
+            } else if (shouldPushBecauseTooFarAway(lane, turnContainer)) {
+                action = Action.PUSH;
             }
         } else {
-            mov = turnContainer.getPathFinder().findPath(self, retreatX, retreatY);
+            Unit avantgard = findAllyAvantgardMinionOrBuilding(lane, turnContainer);
+            Point waypointAvant = mapUtils.pushWaypoint(avantgard.getX(), avantgard.getY(), lane);
+            if (avantgard.getDistanceTo(waypointAvant.getX(), waypointAvant.getY()) >
+                    self.getDistanceTo(waypointAvant.getX(), waypointAvant.getY())) {
+                action = Action.RETREAT;
+            } else {
+                action = Action.PUSH;
+            }
+        }
+
+        Point pushWaypoint = mapUtils.pushWaypoint(self.getX(), self.getY(), lane);
+        Point retreatWaypoint = mapUtils.retreatWaypoint(self.getX(), self.getY(), lane);
+        Movement mov;
+
+        switch (action) {
+            case NONE:
+                return Optional.empty();
+            case RETREAT:
+                mov = turnContainer.getPathFinder().findPath(self, retreatWaypoint.getX(), retreatWaypoint.getY());
+                break;
+            case PUSH:
+                mov = turnContainer.getPathFinder().findPath(self, pushWaypoint.getX(), pushWaypoint.getY());
+                break;
+            default:
+                throw new RuntimeException("Unexpected action " + action);
         }
 
         MoveBuilder moveBuilder = new MoveBuilder();
@@ -69,23 +62,98 @@ public class PushLaneTacticBuilder implements TacticBuilder {
         return Optional.of(new TacticImpl("PushLane", moveBuilder, Tactics.PUSH_LANE_TACTIC_PRIORITY));
     }
 
-    private Optional<Unit> findClosestOffensiveUnit(LocationType lane, TurnContainer turnContainer) {
+    private boolean shouldPushBecauseTooFarAway(LocationType lane, TurnContainer turnContainer) {
+        for (Unit unit : turnContainer.getWorldProxy().allUnitsWoTrees()) {
+            if (!(unit instanceof Minion || unit instanceof Building) || !turnContainer.isOffensiveUnit(unit)) {
+                continue;
+            }
+            double dist = turnContainer.getSelf().getDistanceTo(unit);
+            if (dist < ENEMY_MIN_DIST_VS_XP_RANGE) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean shouldRetreatBecauseOfMinions(LocationType lane, TurnContainer turnContainer) {
+        for (Unit unit : turnContainer.getWorldProxy().allUnitsWoTrees()) {
+            if (!(unit instanceof Minion) || !turnContainer.isOffensiveUnit(unit)) {
+                continue;
+            }
+            double dist = turnContainer.getSelf().getDistanceTo(unit);
+            if (dist < ENEMY_MINION_MAX_DIST) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean shouldRetreatBecauseOfBuildings(LocationType lane, TurnContainer turnContainer) {
+        Building building = null;
+        for (Unit unit : turnContainer.getWorldProxy().allUnitsWoTrees()) {
+            if (!(unit instanceof Building) || !turnContainer.isOffensiveUnit(unit)) {
+                continue;
+            }
+            double dist = turnContainer.getSelf().getDistanceTo(unit);
+            if (dist <=
+                    ((Building) unit).getAttackRange() - turnContainer.getSelf().getRadius() + TOWER_DANGER_RANGE_INC) {
+                building = (Building) unit;
+            }
+        }
+        if (building == null) {
+            return false;
+        }
+        int c = 0;
+        for (Unit unit : turnContainer.getWorldProxy().allUnitsWoTrees()) {
+            if (turnContainer.isAllyUnit(unit) && building.getDistanceTo(unit) <=
+                    building.getAttackRange() - ((CircularUnit) unit).getRadius() + TOWER_DANGER_RANGE_INC) {
+                c++;
+            }
+        }
+        return c <= TOWER_TARGETS_THRESHOLD;
+    }
+
+    private Unit findAllyAvantgardMinionOrBuilding(LocationType lane, TurnContainer turnContainer) {
         WorldProxy world = turnContainer.getWorldProxy();
-        Wizard self = turnContainer.getSelf();
+        MapUtils mapUtils = turnContainer.getMapUtils();
+        Point allyPoint = mapUtils.getLaneAllyWaypoint();
+
+        Optional<Unit> avantgardEnemy = findEnemyAvantgardMinionOrBuilding(lane, turnContainer);
+
+        double bestDist = Double.MAX_VALUE;
+        Unit bestUnit = null;
+        for (Unit unit : world.allUnitsWoTrees()) {
+            LocationType curLane = mapUtils.getLocationType(unit.getId());
+            if (!(curLane == lane || curLane == LocationType.ALLY_BASE || curLane == LocationType.ENEMY_BASE) ||
+                    !(unit instanceof Building || unit instanceof Minion) || !turnContainer.isAllyUnit(unit)) {
+                continue;
+            }
+            double dist =
+                    unit.getDistanceTo(mapUtils.getLaneEnemyWaypoint().getX(), mapUtils.getLaneEnemyWaypoint().getY());
+            if (dist < bestDist && (!avantgardEnemy.isPresent() ||
+                    unit.getDistanceTo(allyPoint.getX(), allyPoint.getY()) <
+                            avantgardEnemy.get().getDistanceTo(allyPoint.getX(), allyPoint.getY()))) {
+                bestDist = dist;
+                bestUnit = unit;
+            }
+        }
+        return bestUnit == null ? world.allyBase() : bestUnit;
+    }
+
+    private Optional<Unit> findEnemyAvantgardMinionOrBuilding(LocationType lane, TurnContainer turnContainer) {
+        WorldProxy world = turnContainer.getWorldProxy();
         MapUtils mapUtils = turnContainer.getMapUtils();
 
         double bestDist = Double.MAX_VALUE;
         Unit bestUnit = null;
         for (Unit unit : world.allUnitsWoTrees()) {
             LocationType curLane = mapUtils.getLocationType(unit.getId());
-            if (curLane != lane && curLane != LocationType.ALLY_BASE && curLane != LocationType.ENEMY_BASE) {
+            if (curLane != lane || !(unit instanceof Building || unit instanceof Minion) ||
+                    !turnContainer.isOffensiveUnit(unit)) {
                 continue;
             }
-            if (!turnContainer.isOffensiveUnit(unit)) {
-                continue;
-            }
-
-            double dist = unit.getDistanceTo(self);
+            double dist =
+                    unit.getDistanceTo(mapUtils.getLaneAllyWaypoint().getX(), mapUtils.getLaneAllyWaypoint().getY());
             if (dist < bestDist) {
                 bestDist = dist;
                 bestUnit = unit;
@@ -94,33 +162,17 @@ public class PushLaneTacticBuilder implements TacticBuilder {
         return Optional.ofNullable(bestUnit);
     }
 
-    private Optional<Unit> findClosestAllyUnit(Point toPoint, LocationType lane, TurnContainer turnContainer) {
-        WorldProxy world = turnContainer.getWorldProxy();
-        Wizard self = turnContainer.getSelf();
-        MapUtils mapUtils = turnContainer.getMapUtils();
-        Faction allyFaction = self.getFaction();
-
-        double bestDist = Double.MAX_VALUE;
-        Unit bestUnit = null;
-        for (Unit unit : world.allUnitsWoTrees()) {
-            LocationType curLane = mapUtils.getLocationType(unit.getId());
-            if (curLane != lane && curLane != LocationType.ALLY_BASE && curLane != LocationType.ENEMY_BASE) {
-                continue;
-            }
-            if (!turnContainer.isAllyUnit(unit)) {
-                continue;
-            }
-
-            double dist = unit.getDistanceTo(toPoint.getX(), toPoint.getY());
-            if (dist < bestDist) {
-                bestDist = dist;
-                bestUnit = unit;
-            }
-        }
-        return Optional.ofNullable(bestUnit);
+    private boolean hasEnemyInVisibilityRange(TurnContainer turnContainer) {
+        return hasEnemyInRange(turnContainer, turnContainer.getSelf().getVisionRange());
     }
+
 
     private boolean hasEnemyInAttackRange(TurnContainer turnContainer) {
+        return hasEnemyInRange(turnContainer,
+                WizardTraits.getWizardCastRange(turnContainer.getSelf(), turnContainer.getGame()));
+    }
+
+    private boolean hasEnemyInRange(TurnContainer turnContainer, double range) {
         Wizard self = turnContainer.getSelf();
         Faction opposingFaction = turnContainer.opposingFaction();
         for (Unit unit : turnContainer.getWorldProxy().allUnitsWoTrees()) {
@@ -128,14 +180,16 @@ public class PushLaneTacticBuilder implements TacticBuilder {
                 continue;
             }
             double dist = unit.getDistanceTo(self);
-            if (dist < WizardTraits.getWizardCastRange(self, turnContainer.getGame())) {
+            if (dist < range) {
                 return true;
             }
         }
         return false;
     }
 
-    private static double dist(double x1, double y1, double x2, double y2) {
-        return Math.hypot(x1 - x2, y1 - y2);
+    private enum Action {
+        RETREAT,
+        PUSH,
+        NONE
     }
 }
