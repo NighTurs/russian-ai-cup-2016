c23f1fdbbd40be3cf1e5d8b4f0e26799ff4918a7
NighTurs <nighturs@gmail.com>
2016-11-19 22:44:34 +0300

    Consider haste bonus while calculating max speed and turn

--- src/main/java/WizardTraits.java
+++ src/main/java/WizardTraits.java
@@ -1,4 +1,6 @@
 import model.Game;
+import model.Status;
+import model.StatusType;
 import model.Wizard;
 
 public final class WizardTraits {
@@ -8,19 +10,19 @@ public final class WizardTraits {
     }
 
     public static double getWizardForwardSpeed(Wizard wizard, Game game) {
-        return game.getWizardForwardSpeed();
+        return movementFactor(wizard, game) * game.getWizardForwardSpeed();
     }
 
     public static double getWizardBackwardSpeed(Wizard wizard, Game game) {
-        return game.getWizardBackwardSpeed();
+        return movementFactor(wizard, game) * game.getWizardBackwardSpeed();
     }
 
     public static double getWizardStrafeSpeed(Wizard wizard, Game game) {
-        return game.getWizardStrafeSpeed();
+        return movementFactor(wizard, game) * game.getWizardStrafeSpeed();
     }
 
     public static double getWizardMaxTurnAngle(Wizard wizard, Game game) {
-        return game.getWizardMaxTurnAngle();
+        return (hasHasteBonus(wizard) ? 1 + game.getHastenedRotationBonusFactor() : 1) * game.getWizardMaxTurnAngle();
     }
 
     public static double getWizardCastRange(Wizard wizard, Game game) {
@@ -29,5 +31,18 @@ public final class WizardTraits {
 
     public static double getWizardCastSector(Game game) {
         return game.getStaffSector() / 2;
+    }
+
+    private static double movementFactor(Wizard wizard, Game game) {
+        return hasHasteBonus(wizard) ? 1 + game.getHastenedMovementBonusFactor() : 1;
+    }
+
+    private static boolean hasHasteBonus(Wizard wizard) {
+        for (Status status : wizard.getStatuses()) {
+            if (status.getType() == StatusType.HASTENED) {
+                return true;
+            }
+        }
+        return false;
     }
 }
