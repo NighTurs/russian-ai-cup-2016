d262003cf10525215682ca6d1b1e7a09b1d5a192
NighTurs <nighturs@gmail.com>
2016-12-07 22:11:00 +0300

    More correct way of keeping 90 angle to enemy wizard

--- src/main/java/NeutralTurnTacticBuilder.java
+++ src/main/java/NeutralTurnTacticBuilder.java
@@ -2,16 +2,33 @@ import java.util.Optional;
 
 public class NeutralTurnTacticBuilder implements TacticBuilder {
 
+    private static final int IGNORE_WIZARD_RANGE = 800;
+
     @Override
     public Optional<Tactic> build(TurnContainer turnContainer) {
         MapUtils mapUtils = turnContainer.getMapUtils();
         WizardProxy self = turnContainer.getSelf();
-        LocationType lane = turnContainer.getLanePicker().myLane();
-        Point retreatWaypoint = mapUtils.retreatWaypoint(self.getX(), self.getY(), lane);
+
+        WizardProxy closestEnemy = null;
+        double minDist = Double.MAX_VALUE;
+        for (WizardProxy wizard : turnContainer.getWorldProxy().getWizards()) {
+            if (!turnContainer.isOffensiveWizard(wizard)) {
+                continue;
+            }
+            if (minDist > self.getDistanceTo(wizard)) {
+                minDist = self.getDistanceTo(wizard);
+                closestEnemy = wizard;
+            }
+        }
+
+        if (closestEnemy == null || minDist > IGNORE_WIZARD_RANGE) {
+            return Optional.empty();
+        }
+
         MoveBuilder moveBuilder = new MoveBuilder();
-        double currentAngle = self.getAngleTo(retreatWaypoint.getX(), retreatWaypoint.getY());
-        if (Math.abs(currentAngle) > Math.PI / 2) {
-            moveBuilder.setTurn(self.getAngleTo(retreatWaypoint.getX(), retreatWaypoint.getY()));
+        double currentAngle = self.getAngleTo(closestEnemy.getX(), closestEnemy.getY());
+        if (Math.abs(currentAngle) < Math.PI / 2) {
+            moveBuilder.setTurn(-self.getAngleTo(closestEnemy.getX(), closestEnemy.getY()));
             return Optional.of(new TacticImpl("NeutralTurn", moveBuilder, Tactics.NEUTRAL_TURN_TACTIC_BUILDER));
         } else {
             return Optional.empty();
