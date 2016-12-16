69f3ad09f14aa30fc189102cd7d639f7cc53f80e
NighTurs <nighturs@gmail.com>
2016-12-16 05:15:10 +0300

    Try counter Recar with 230

--- src/main/java/MasterWizardTacticBuilder.java
+++ src/main/java/MasterWizardTacticBuilder.java
@@ -1,12 +1,16 @@
-import model.LaneType;
-import model.Message;
-import model.Unit;
+import model.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class MasterWizardTacticBuilder implements TacticBuilder {
 
+    private static final Set<String> SCARED_OF = new HashSet<>(Collections.singletonList("Recar"));
+    private static final List<LaneRole> ALL_MID = Arrays.asList(new LaneRole(LaneType.MIDDLE, WizardRole.FROST_BOLT),
+            new LaneRole(LaneType.MIDDLE, WizardRole.HASTE_QUICK),
+            new LaneRole(LaneType.MIDDLE, WizardRole.RANGE),
+            new LaneRole(LaneType.MIDDLE, WizardRole.SHIELD_QUICK),
+            new LaneRole(LaneType.MIDDLE, WizardRole.FIREBALL_TEAM));
     public static final byte ROLE_MESSAGE_CODE = 'r';
     private static final int NUMBER_OF_ALLY_WIZARDS = 4;
 
@@ -35,18 +39,33 @@ public class MasterWizardTacticBuilder implements TacticBuilder {
         if (world.getTickIndex() == 0) {
             MoveBuilder moveBuilder = new MoveBuilder();
             Message[] messages = new Message[NUMBER_OF_ALLY_WIZARDS];
-            Queue<WizardRole> roles = new ArrayDeque<>(Arrays.asList(WizardRole.FROST_BOLT,
-                    WizardRole.HASTE,
-                    WizardRole.RANGE,
-                    WizardRole.SHIELD,
-                    WizardRole.FIREBALL_TEAM));
+
+            boolean scared = false;
+            for (Player player : world.getPlayers()) {
+                if (SCARED_OF.contains(player.getName())) {
+                    scared = true;
+                }
+            }
+
+            Queue<LaneRole> roles;
+            if (scared) {
+                LaneType edgeLane = self.getFaction() == Faction.ACADEMY ? LaneType.TOP : LaneType.BOTTOM;
+                roles = new ArrayDeque<>(Arrays.asList(new LaneRole(LaneType.MIDDLE, WizardRole.RANGE),
+                        new LaneRole(LaneType.MIDDLE, WizardRole.HASTE_DELAYED),
+                        new LaneRole(LaneType.MIDDLE, WizardRole.SHIELD_DELAYED),
+                        new LaneRole(edgeLane, WizardRole.FIREBALL_SOLO),
+                        new LaneRole(edgeLane, WizardRole.FIREBALL_SOLO)));
+            } else {
+                roles = new ArrayDeque<>(ALL_MID);
+            }
             for (WizardProxy wizard : allyWizardsExceptMe) {
-                WizardRole role = roles.poll();
+                LaneRole laneRole = roles.poll();
                 messages[memory.getAllyWizardMessageIndex().get(wizard.getId())] =
-                        new Message(LaneType.MIDDLE, null, roleMessage(role));
+                        new Message(laneRole.laneType, null, roleMessage(laneRole.role));
             }
             moveBuilder.setMessages(messages);
-            turnContainer.getMemory().setSelfMessage(new Message(LaneType.MIDDLE, null, roleMessage(roles.poll())));
+            LaneRole laneRole = roles.poll();
+            turnContainer.getMemory().setSelfMessage(new Message(laneRole.laneType, null, roleMessage(laneRole.role)));
             return Optional.of(new TacticImpl("MasterWizard", moveBuilder, Tactics.MASTER_WIZARD_TACTIC_BUILDER));
         }
         return Optional.empty();
@@ -54,5 +73,16 @@ public class MasterWizardTacticBuilder implements TacticBuilder {
 
     private byte[] roleMessage(WizardRole role) {
         return new byte[]{ROLE_MESSAGE_CODE, (byte) role.getCode()};
+    }
+
+    private static class LaneRole {
+
+        LaneType laneType;
+        WizardRole role;
+
+        public LaneRole(LaneType laneType, WizardRole role) {
+            this.laneType = laneType;
+            this.role = role;
+        }
     }
 }
