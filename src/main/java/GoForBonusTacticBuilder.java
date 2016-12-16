abebe58446c8bdc50ca138f3500d8a5fc7004816
NighTurs <nighturs@gmail.com>
2016-12-16 03:08:06 +0300

    Increase bonus allowed time constant

--- src/main/java/GoForBonusTacticBuilder.java
+++ src/main/java/GoForBonusTacticBuilder.java
@@ -7,7 +7,7 @@ public class GoForBonusTacticBuilder implements TacticBuilder {
     private static final int ALLY_HALF_MANHATTAN_BASES = 9;
     private static final int ARRIVE_BEFORE_TICKS = 80;
     private static final int EXPECTED_TICKS_TO_BONUS_ERROR = 400;
-    private static final int ACCEPTABLE_TICKS_TO_TAKE_BONUS_WITH_TEAM = 600;
+    private static final int ACCEPTABLE_TICKS_TO_TAKE_BONUS_WITH_TEAM = 800;
     private static final int ACCEPTABLE_TICKS_TO_TAKE_BONUS_WITH_SKILLS = 1000;
     private static final int ACCEPTABLE_TICKS_TO_TAKE_BONUS_WITHOUT_SKILLS = 1300;
     private static final int KEEP_DISTANCE_TO_BONUS = 2;
